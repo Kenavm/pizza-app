a9c90ae8983b014ad8fc279afa6790c3b11297d7
Manuel <manuel.vanek@gmail.com>
2023-02-18 16:18:26 +0100

    added fetchPizzaData

--- pizza-frontend/src/App.jsx
+++ pizza-frontend/src/App.jsx
@@ -1,19 +1,46 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<div className="header-main">
-				<img id="site-logo" src="../images/logo-pizza.jpg"></img>
-			</div>
-			<div className="body-main"></div>
-			<div id="Searchbox-container"></div>
-			<div id="Result-container"></div>
-			<div id="Cart-container"></div>
-			<div className="footer-main"></div>
-		</div>
-	);
+  const [pizzaData, setPizzaData] = useState();
+
+  const [count, setCount] = useState(0);
+
+  const fetchPizzaData = async () => {
+    const res = await fetch("http://localhost:3000/api/pizzas");
+    let data = await res.json();
+    setPizzaData(data);
+   
+  };
+  
+  fetchPizzaData();
+ 
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
 }
 
 export default App;
