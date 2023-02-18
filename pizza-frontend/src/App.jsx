760a60d4d51a825d03e11790cbc90805e58476b5
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-02-18 16:22:49 +0100

    basic layout

--- pizza-frontend/src/App.jsx
+++ pizza-frontend/src/App.jsx
@@ -7,11 +7,26 @@ function App() {
 			<div className="header-main">
 				<img id="site-logo" src="../images/logo-pizza.jpg"></img>
 			</div>
-			<div className="body-main"></div>
-			<div id="Searchbox-container"></div>
-			<div id="Result-container"></div>
-			<div id="Cart-container"></div>
-			<div className="footer-main"></div>
+			<div className="body-main">
+				<div id="Searchbox-container">
+					<h2>Search for the pizza of your dreams!</h2>
+				</div>
+				<div id="Result-container">
+					<h2>
+						<u>Results</u>
+					</h2>
+				</div>
+				<div id="Cart-container">
+					<h2>Cart</h2>
+					<img id="cart-icon" src="../images/cart.png"></img>
+				</div>
+			</div>
+
+			<div className="footer-main">
+				<h3>
+					<u>Order summary</u>
+				</h3>
+			</div>
 		</div>
 	);
 }
