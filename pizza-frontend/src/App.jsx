73cb0be73b5b58d21277f577797832870589cfbc
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-02-19 17:05:12 +0100

    layout auskommentiert

--- pizza-frontend/src/App.jsx
+++ pizza-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import PizzaList from "./component/PizzaList";
 import PriceFilter from "./component/PriceFilter";
-import data from "./api/fetchPizza";
+//import data from "./api/fetchPizza";
 
 let filtered = false;
 
@@ -12,29 +12,29 @@ function App() {
 	const [pizzaData, setPizzaData] = useState([]);
 	const [priceFilter, setPriceFilter] = useState([]);
 
-	useEffect(() => {
-		setPizzaData(data);
-	}, []);
+	// useEffect(() => {
+	// 	setPizzaData(data);
+	// }, []);
 
-	function filterByPrice(min, max) {
-		if (min !== "" && max !== "") {
-			filtered = true;
-			setPriceFilter(
-				pizzaData.filter(
-					(pizza) => pizza.price > parseInt(min) && pizza.price <= parseInt(max)
-				)
-			);
-		} else if (min === "" && max !== "") {
-			filtered = true;
-			setPriceFilter(pizzaData.filter((pizza) => pizza.price <= parseInt(max)));
-		} else if (min !== "" && max === "") {
-			filtered = true;
-			setPriceFilter(pizzaData.filter((pizza) => pizza.price >= parseInt(min)));
-		} else {
-			setPriceFilter(pizzaData);
-			filtered = false;
-		}
-	}
+	// function filterByPrice(min, max) {
+	// 	if (min !== "" && max !== "") {
+	// 		filtered = true;
+	// 		setPriceFilter(
+	// 			pizzaData.filter(
+	// 				(pizza) => pizza.price > parseInt(min) && pizza.price <= parseInt(max)
+	// 			)
+	// 		);
+	// 	} else if (min === "" && max !== "") {
+	// 		filtered = true;
+	// 		setPriceFilter(pizzaData.filter((pizza) => pizza.price <= parseInt(max)));
+	// 	} else if (min !== "" && max === "") {
+	// 		filtered = true;
+	// 		setPriceFilter(pizzaData.filter((pizza) => pizza.price >= parseInt(min)));
+	// 	} else {
+	// 		setPriceFilter(pizzaData);
+	// 		filtered = false;
+	// 	}
+	// }
 
 	console.log(pizzaData);
 	return (
@@ -45,7 +45,7 @@ function App() {
 			<div className="body-main">
 				<div id="Searchbox-container">
 					<h2>Search for the pizza of your dreams!</h2>
-					<PriceFilter filterByPrice={filterByPrice} />
+					{/*<PriceFilter filterByPrice={filterByPrice} />*/}
 				</div>
 				<div id="Result-container">
 					<h2>
