d3ad3b36bdbe3dae3c974324a45bb29e02451c12
Manuel <manuel.vanek@gmail.com>
2023-02-19 20:14:32 +0100

    move filter logic to backend

--- pizza-frontend/src/App.jsx
+++ pizza-frontend/src/App.jsx
@@ -1,71 +1,67 @@
 import NameFilter from "./component/NameFilter";
 import { useState, useEffect } from "react";
-
+import fetchPriceFilter from "./api/fetchFilteredDataByPrice";
 import "./App.css";
 import PizzaList from "./component/PizzaList";
 import PriceFilter from "./component/PriceFilter";
-//import data from "./api/fetchPizza";
+import data from "./api/fetchPizza";
 
 let filtered = false;
 
 function App() {
-	const [pizzaData, setPizzaData] = useState([]);
-	const [priceFilter, setPriceFilter] = useState([]);
+  const [pizzaData, setPizzaData] = useState([]);
+  const [priceFilter, setPriceFilter] = useState([]);
 
-	// useEffect(() => {
-	// 	setPizzaData(data);
-	// }, []);
+  useEffect(() => {
+    setPizzaData(data);
+  }, []);
+  
+    async function filterByPrice(min, max) {
+      if (min === "" && max === "") {
+        setPriceFilter(pizzaData);
+        filtered = false;
+      } else {
+        let filteredData = await fetchPriceFilter(min, max);
+        filtered = true;
+		console.log(filteredData)
+		
+        setPriceFilter(filteredData);
+      }
+    }
+	useEffect(() => {
+		console.log(priceFilter);
+	  }, [priceFilter]);
 
-	// function filterByPrice(min, max) {
-	// 	if (min !== "" && max !== "") {
-	// 		filtered = true;
-	// 		setPriceFilter(
-	// 			pizzaData.filter(
-	// 				(pizza) => pizza.price > parseInt(min) && pizza.price <= parseInt(max)
-	// 			)
-	// 		);
-	// 	} else if (min === "" && max !== "") {
-	// 		filtered = true;
-	// 		setPriceFilter(pizzaData.filter((pizza) => pizza.price <= parseInt(max)));
-	// 	} else if (min !== "" && max === "") {
-	// 		filtered = true;
-	// 		setPriceFilter(pizzaData.filter((pizza) => pizza.price >= parseInt(min)));
-	// 	} else {
-	// 		setPriceFilter(pizzaData);
-	// 		filtered = false;
-	// 	}
-	// }
+  console.log(pizzaData);
+  return (
+    <div className="App">
+      <div className="header-main">
+        <img id="site-logo" src="../images/logo-pizza.jpg"></img>
+      </div>
+      <div className="body-main">
+        <div id="Searchbox-container">
+          <h2>Search for the pizza of your dreams!</h2>
+          {<PriceFilter filterByPrice={filterByPrice} />}
+        </div>
+        <div id="Result-container">
+          <h2>
+            <u>Results</u>
+            <PizzaList pizzas={filtered ? priceFilter : pizzaData}></PizzaList>;
+          </h2>
+        </div>
+        <div id="Cart-container">
+          <h2>Cart</h2>
+          <img id="cart-icon" src="../images/cart.png"></img>
+        </div>
+      </div>
 
-	console.log(pizzaData);
-	return (
-		<div className="App">
-			<div className="header-main">
-				<img id="site-logo" src="../images/logo-pizza.jpg"></img>
-			</div>
-			<div className="body-main">
-				<div id="Searchbox-container">
-					<h2>Search for the pizza of your dreams!</h2>
-					{/*<PriceFilter filterByPrice={filterByPrice} />*/}
-				</div>
-				<div id="Result-container">
-					<h2>
-						<u>Results</u>
-						<PizzaList pizzas={filtered ? priceFilter : pizzaData}></PizzaList>;
-					</h2>
-				</div>
-				<div id="Cart-container">
-					<h2>Cart</h2>
-					<img id="cart-icon" src="../images/cart.png"></img>
-				</div>
-			</div>
-
-			<div className="footer-main">
-				<h3>
-					<u>Order summary</u>
-				</h3>
-			</div>
-		</div>
-	);
+      <div className="footer-main">
+        <h3>
+          <u>Order summary</u>
+        </h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ pizza-frontend/src/api/fetchFilteredDataByPrice.js
@@ -0,0 +1,9 @@
+async function fetchPriceFilter(min, max) {
+
+    const url = `http://localhost:3000/api/pizzas?min-price=${min}&max-price=${max}`;
+    let res = await fetch(url);
+    let filteredData = await res.json();
+    return filteredData;
+}
+
+export default fetchPriceFilter
