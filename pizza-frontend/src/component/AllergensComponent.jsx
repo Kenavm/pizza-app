d790acacaad76f25df199dd86f394e2d12f930e4
Katarzyna Pastula <katarzyna.pastula@outlook.com>
2023-02-21 10:39:53 +0100

    added label for allergens in AllergensComponent

--- pizza-frontend/src/component/AllergensComponent.jsx
+++ pizza-frontend/src/component/AllergensComponent.jsx
@@ -1,10 +1,15 @@
-export default function AllergensComponent( props ) {
+export default function AllergensComponent(props) {
     return (
-        <form>
-        { props.allergens.map((allergen) => {(
-        <input type="checkbox" key={allergen.letter} name={allergen.name} value={allergen.name}/>
-        )})
-        }
-        </form>
-    )
-}
+      <form>
+        {props.allergens.map((allergen) => (
+          <><input
+            type="checkbox"
+            key={allergen.letter}
+            name={allergen.name}
+            value={allergen.name} />
+            <label for={allergen.name}>{allergen.name}</label><br/></>
+        ))}
+      </form>
+    );
+  }
+  
