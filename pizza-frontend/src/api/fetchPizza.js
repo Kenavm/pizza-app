a62d35987808f12831c411b06883fc7429373af1
Manuel <manuel.vanek@gmail.com>
2023-02-23 12:45:26 +0100

    small changes

--- pizza-frontend/src/api/fetchPizza.js
+++ pizza-frontend/src/api/fetchPizza.js
@@ -1,19 +1,23 @@
 const fetchPizzaData = async (parameters) => {
   let baseUrl = "http://localhost:3000/api/pizzas";
   let url;
-  console.log(parameters.allergen)
-  console.log(parameters.maxPrice)
-  console.log(parameters.pizzaName)
-  if (parameters.minPrice !== undefined && parameters.maxPrice !== undefined) {
-    console.log("test")
-    if (parameters.minPrice !== "" && parameters.maxPrice !== "") {
-      url = `${baseUrl}?min-price=${parameters.minPrice}&max-price=${parameters.maxPrice}`;
-    }
+  console.log(parameters.allergen);
+  console.log(parameters.maxPrice);
+  console.log(parameters.pizzaName);
+  if (
+    parameters.minPrice !== undefined &&
+    parameters.maxPrice !== undefined &&
+    parameters.minPrice !== "" &&
+    parameters.maxPrice !== ""
+  ) {
+    console.log("test");
+
+    url = `${baseUrl}?min-price=${parameters.minPrice}&max-price=${parameters.maxPrice}`;
   } else if (parameters.allergen !== undefined) {
-    console.log("allergen name")
+    console.log("allergen name");
     url = baseUrl + `?avoid-allergen-by-name=${parameters.allergen.name}`;
   } else if (parameters.pizzaName !== undefined) {
-    console.log("test name")
+    console.log("test name");
     url = baseUrl + `?name=${parameters.pizzaName}`;
     //} else if (parameter.sortBy === /*whatever the condition will be*/) {
     //url+`?sort-asc=${parameter.sortBy}`
