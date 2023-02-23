eb0b88ff0cb494b369a7e0f2b88f8d1e749b949a
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-02-23 19:10:53 +0100

    responsive layout

--- pizza-frontend/src/component/NameFilter.jsx
+++ pizza-frontend/src/component/NameFilter.jsx
@@ -1,45 +1,23 @@
 import "./NameFilter.css";
 
 export default function NameFilter(props) {
-  // let filteredData = fetchPizzaData();
-  // const prom = Promise.resolve(filteredData);
+	// let filteredData = fetchPizzaData();
+	// const prom = Promise.resolve(filteredData);
 
-<<<<<<< HEAD
-	function handleSubmit(event) {
-		event.preventDefault();
-		props.onNameInput(userInput);
-	}
+	// prom.then((value) => {
+	// 	console.log(value);
+
+	// 	const nameArray = value.map((obj) => obj.name);
+
+	// 	console.log(nameArray);
+	// });
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					onChange={(e) => setUserInput(userInput)}
-					value={userInput}
-					placeholder="Type in pizza name"
-				></input>
-				<button type="submit" className="filter-button">
-					Filter
-				</button>
-			</form>
+			<input
+				onChange={(e) => props.isSetName(e.target.value)}
+				placeholder="Type in pizza name"
+			></input>
 		</div>
 	);
-=======
-  // prom.then((value) => {
-  // 	console.log(value);
-
-  // 	const nameArray = value.map((obj) => obj.name);
-
-  // 	console.log(nameArray);
-  // });
-
-  return (
-    <div>
-      <input
-        onChange={(e) => props.isSetName(e.target.value)}
-        placeholder="Type in pizza name"
-      ></input>
-    </div>
-  );
->>>>>>> f7da5594f66dd2cef540e1db71041b7ba0b0ba9c
 }
--- pizza-frontend/src/features/filters/Filters.jsx
+++ pizza-frontend/src/features/filters/Filters.jsx
@@ -2,49 +2,48 @@ import NameFilter from "../../component/NameFilter";
 import PriceFilter from "../../component/PriceFilter";
 import AllergensFilter from "../../component/AllergensFilter";
 import Button from "../../component/ButtonComponent";
-import './Filters.css'
+import "./Filters.css";
 import { useState } from "react";
 import fetchPizzaData from "../../api/fetchPizza";
 
 function Filters(props) {
+	const [minPrice, setMinPrice] = useState();
+	const [maxPrice, setMaxPrice] = useState();
+	const [name, setName] = useState();
+	const [allergen, setAllergen] = useState();
 
-  const [minPrice, setMinPrice] = useState();
-  const [maxPrice, setMaxPrice] = useState();
-  const [name, setName] = useState();
-  const [allergen, setAllergen] = useState();
+	async function filterPizzas() {
+		let allergenToFilter = props.allergenData.filter(
+			(allergenFromData) => allergenFromData.name === allergen
+		);
 
-  async function filterPizzas() {
-    let allergenToFilter = props.allergenData.filter(allergenFromData => allergenFromData.name === allergen)
-  
-    let filteredData = await fetchPizzaData({
-      pizzaName: name,
-      minPrice: minPrice,
-      maxPrice: maxPrice,
-      allergen: allergenToFilter[0],
-    });
+		let filteredData = await fetchPizzaData({
+			pizzaName: name,
+			minPrice: minPrice,
+			maxPrice: maxPrice,
+			allergen: allergenToFilter[0],
+		});
 
-    props.isSetPizzaData(filteredData);
-  }
+		props.isSetPizzaData(filteredData);
+	}
 
-  return (
-    <div className="filter-container">
-      <NameFilter isSetName={setName} />
+	return (
+		<div className="filter-container">
+			<NameFilter isSetName={setName} />
 
-      <PriceFilter
-        isSetMinPrice={setMinPrice}
-        isSetMaxPrice={setMaxPrice}
-      />
-      <AllergensFilter
-        isSetAllergen={setAllergen}
-        allergenData={props.allergenData}
-      />
-      <Button
-        onClick={() => filterPizzas()}
-        buttonClassName={"filter-button"}
-        buttonName={"Filter"}
-      />
-    </div>
-  );
+			<PriceFilter isSetMinPrice={setMinPrice} isSetMaxPrice={setMaxPrice} />
+			<AllergensFilter
+				isSetAllergen={setAllergen}
+				allergenData={props.allergenData}
+			/>
+			<br />
+			<Button
+				onClick={() => filterPizzas()}
+				buttonClassName={"filter-button"}
+				buttonName={"Filter"}
+			/>
+		</div>
+	);
 }
 
 export default Filters;
