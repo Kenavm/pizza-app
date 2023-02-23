e74114bb7fe0fa3653b6ed4b92ff611a17556057
Manuel <manuel.vanek@gmail.com>
2023-02-23 11:13:40 +0100

    cleanup

--- pizza-frontend/src/api/fetchDataFilteredByAllergen.js
+++
@@ -1,8 +0,0 @@
-async function fetchDataFilteredByAllergen({allergenToFilter}) {
-    const url = `http://localhost:3000/api/pizzas?avoid-allergen-by-name=${allergenToFilter}`;
-    let res = await fetch(url);
-    let filteredData = await res.json();
-    return filteredData;
-}
-
-export default fetchDataFilteredByAllergen
