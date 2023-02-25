25aa6f3de068cd4875c4ce1172a23d8890483397
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-02-25 10:26:15 +0100

    Cart Layout

--- pizza-frontend/src/component/Cart.jsx
+++ pizza-frontend/src/component/Cart.jsx
@@ -1,15 +1,39 @@
 import "./Cart.css";
 
+import ButtonComponent from "./ButtonComponent";
+
 export default function Cart(props) {
 	return (
 		<div>
 			{props.cartContents.map((pizza) => {
 				return (
 					<div id={pizza.id}>
-						<p>Name: {pizza.name}</p>
-						<p>Price: {pizza.price}€</p>
-						<p>Amount: {pizza.amount}x</p>
-						<button onClick={() => props.onDelete(pizza.id)}>Delete</button>
+						<table>
+							<tr>
+								<td>
+									<b>Name:</b>
+								</td>
+								<td>{pizza.name}</td>
+							</tr>
+							<tr>
+								<td>
+									<b>Price:</b>{" "}
+								</td>
+								<td>{pizza.price}€</td>
+							</tr>
+							<tr>
+								<td>
+									<b>Amount: </b>
+								</td>
+								<td>{pizza.amount}x</td>
+							</tr>
+						</table>
+
+						<ButtonComponent
+							buttonClassName={"filter-button"}
+							buttonName={"Delete"}
+							onClick={() => props.onDelete(pizza.id)}
+						></ButtonComponent>
 					</div>
 				);
 			})}
